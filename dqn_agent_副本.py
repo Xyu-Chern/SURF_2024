9f266a7eede680c040d31de93e37fd9712a68569
charlolo1314 <150693002+charlolo1314@users.noreply.github.com>
2024-07-31 10:21:33 +0800

    Add files via upload

---
+++ dqn_agent_副本.py
@@ -0,0 +1,150 @@
+from typing import Sequence, Callable, Tuple, Optional
+
+import torch
+from torch import nn
+
+import numpy as np
+
+import cs285.infrastructure.pytorch_util as ptu
+
+
+class DQNAgent(nn.Module):
+    def __init__(
+        self,
+        observation_shape: Sequence[int], num_actions: int,
+        make_critic: Callable[[Tuple[int, ...], int], nn.Module],
+        make_optimizer: Callable[[torch.nn.ParameterList], torch.optim.Optimizer],
+        make_lr_schedule: Callable[
+            [torch.optim.Optimizer], torch.optim.lr_scheduler._LRScheduler
+        ],
+        discount: float,
+        target_update_period: int,
+        use_double_q: bool = False,
+        clip_grad_norm: Optional[float] = None,
+    ):
+        super().__init__()
+
+        self.critic = make_critic(observation_shape, num_actions)
+        self.target_critic = make_critic(observation_shape, num_actions)
+        self.critic_optimizer = make_optimizer(self.critic.parameters())
+        self.lr_scheduler = make_lr_schedule(self.critic_optimizer)
+
+        self.observation_shape = observation_shape
+        self.num_actions = num_actions
+        self.discount = discount
+        self.target_update_period = target_update_period
+        self.clip_grad_norm = clip_grad_norm
+        self.use_double_q = use_double_q
+
+        self.critic_loss = nn.MSELoss()
+
+        self.update_target_critic()
+
+    def get_action(self, observation: np.ndarray, epsilon: float = 0.0) -> int:
+        """
+        Used for evaluation.
+        """
+        observation = ptu.from_numpy(np.asarray(observation))[None]
+
+        
+        
+        if np.random.rand() < epsilon:
+            action = np.random.randint(self.num_actions)
+        else:
+            with torch.no_grad():
+                q_values =self.critic(observation)
+                action = torch.argmax(q_values,dim=1).item()
+
+        return action
+
+    def compute_critic_loss(
+        self,
+        obs: torch.Tensor,
+        action: torch.Tensor,
+        reward: torch.Tensor,
+        next_obs: torch.Tensor,
+        done: torch.Tensor,
+    ) -> Tuple[torch.Tensor, dict, dict]:
+        """
+        Compute the loss for the DQN critic.
+
+        Returns:
+         - loss: torch.Tensor, the MSE loss for the critic
+         - metrics: dict, a dictionary of metrics to log
+         - variables: dict, a dictionary of variables that can be used in subsequent calculations
+        """
+
+    
+        batch_size = obs.size(0)
+        q_values = self.critic(obs)
+        qa_values = q_values.gather(1, action.long().unsqueeze(1)).sequeeze(1)
+        
+        with torch.no_grad():
+            next_q_values = self.target_critic(next_obs)
+            if self.use_double_q:
+                next_q_values_critic = self.critic(next_obs)
+                next_actions = torch.argmax(next_q_values_critic,dim=1)
+                next_q_values = next_q_values.gather(1,next_actions.unsqueeze(1)).squeeze(1)
+            else:
+                next_q_values = next_q_values.max(dim=1)[0]
+            target_values = reward + (1-done)*self.discount*next_q_values
+            
+        loss = self.critic_loss(qa_values, target_values)
+        return (
+            loss,
+            {
+                "critic_loss": loss.item(),
+                "q_values": q_values.mean().item(),
+                "target_values": target_values.mean().item(),
+            },
+            {
+                "qa_values": qa_values,
+                "q_values": q_values,
+            },
+        )
+
+    def update_critic(
+        self,
+        obs: torch.Tensor,
+        action: torch.Tensor,
+        reward: torch.Tensor,
+        next_obs: torch.Tensor,
+        done: torch.Tensor,
+    ) -> dict:
+        """Update the DQN critic, and return stats for logging."""
+        loss, metrics, _ = self.compute_critic_loss(obs, action, reward, next_obs, done)
+
+        self.critic_optimizer.zero_grad()
+        loss.backward()
+        grad_norm = torch.nn.utils.clip_grad.clip_grad_norm_(
+            self.critic.parameters(), self.clip_grad_norm or float("inf")
+        )
+        metrics["grad_norm"] = grad_norm.item()
+        self.critic_optimizer.step()
+
+        self.lr_scheduler.step()
+
+        return metrics
+
+    def update_target_critic(self):
+        self.target_critic.load_state_dict(self.critic.state_dict())
+
+    def update(
+        self,
+        obs: torch.Tensor,
+        action: torch.Tensor,
+        reward: torch.Tensor,
+        next_obs: torch.Tensor,
+        done: torch.Tensor,
+        step: int,
+    ) -> dict:
+        """
+        Update the DQN agent, including both the critic and target.
+        """
+       
+        critic_stats = self.update_critic(obs,action,reward,next_obs,done)
+        
+        if step % self.target.update_period ==0:
+            self.update_target_critic()
+        
+        return critic_stats
